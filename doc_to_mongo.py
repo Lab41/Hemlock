b2a693b8a0128c0dbd433611eeb63f260a47d927
Charlie Lewis <clewis@iqt.org>
2013-05-13 23:46:37 +0000

    script for importing into mongo and some sample data for it

---
+++ doc_to_mongo.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python
+
+import fnmatch, os, sys, time, uuid
+from pymongo import MongoClient
+
+def mongo_server(server, port, database, collection):
+    # connect to the redis server
+    try:
+        m_server = MongoClient(server, port)
+        m_database = m_server[database]
+        m_collection = m_database[collection]
+    except:
+        print "Mongo server failure"
+        sys.exit(0)
+    return m_server, m_database, m_collection
+
+def process_doc(input, m_server, m_database, m_collection):
+    matches = []
+    docs = []
+    for root, dirnames, filenames in os.walk(input):
+        for filename in fnmatch.filter(filenames, '*.txt'):
+            matches.append(os.path.join(root, filename))
+    j = 0
+    for file in matches:
+        doc = open(file, 'r').read()
+        doc = {"doc": doc}
+        docs.append(doc)
+        j += 1
+    m_collection.insert(docs)
+    print str(j), "docs added."
+
+def print_help():
+    print "-i \t<input path to files> (default is /mnt/)"
+    print "-s \t<mongo server> (default is localhost)"
+    print "-p \t<mongo port> (default is 27017)"
+    print "-d \t<mongo database> (default is local)"
+    print "-c \t<mongo collection> (default is collection)"
+    print "-h \thelp\n"
+    sys.exit(0)
+
+def process_args(args):
+    # default initialization
+    input = "/mnt/"
+    server = "localhost"
+    port = 27017
+    database = "local"
+    collection = "collection"
+
+    # process args
+    i = 0
+    while i < len(args):
+        if args[i] == "s":
+            try:
+                server = args[i+1]
+                i += 1
+            except:
+                print_help()
+        elif args[i] == "-p":
+            try:
+                port = int(args[i+1])
+                i += 1
+            except:
+                print_help()
+        elif args[i] == "-d":
+            try:
+                database = args[i+1]
+                i += 1
+            except:
+                print_help()
+        elif args[i] == "-c":
+            try:
+                collection = args[i+1]
+                i += 1
+            except:
+                print_help()
+        elif args[i] == "-i":
+            try:
+                input = args[i+1]
+                i += 1
+            except:
+                print_help()
+        else:
+            print_help()
+        i += 1
+    return input, server, port, database, collection
+
+def get_args():
+    args = []
+    for arg in sys.argv:
+        args.append(arg)
+    return args[1:]
+
+if __name__ == "__main__":
+    start_time = time.time()
+    args = get_args()
+    input, server, port, database, collection = process_args(args)
+    m_server, m_database, m_collection = mongo_server(server, port, database, collection)
+    process_doc(input, m_server, m_database, m_collection)
+    print "Took",time.time() - start_time,"seconds to complete."
+
