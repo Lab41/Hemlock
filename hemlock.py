37cc5fddd6f428065af1b117708d73aae0ff24fb
Charlie Lewis <clewis@iqt.org>
2013-05-15 11:41:09 -0700

    template for getting for args for api calls

---
+++ hemlock.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python
+
+import sys, time
+
+HELP_COUNTER = 0
+
+def user_create(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def user_delete(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def user_list(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def user_get(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def tenant_create(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def tenant_delete(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def tenant_list(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def tenant_get(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def system_list(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def system_get(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def register_remote_system(args, var_d):
+    arg_d = [
+        '--name',
+        '--data_type',
+        '--description',
+        '--tenant',
+        '--hostname',
+        '--port',
+        '--poc_name',
+        '--poc_email'
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def deregister_remote_system(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def register_local_system(args, var_d):
+    arg_d = [
+        '--name'
+        '--data_type',
+        '--description',
+        '--tenant',
+        '--hostname',
+        '--endpoint',
+        '--poc_name',
+        '--poc_email'
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def deregister_local_system(args, var_d):
+    arg_d = [
+    ]
+    return check_args(args, arg_d, var_d) 
+
+def check_args(args, arg_d, var_d):
+    global HELP_COUNTER
+    i = 0
+    while i < len(args):
+        try:
+            if args[i] in arg_d:
+                var_d[args[i]] = args[i+1]
+            arg_d.remove(args[i])
+            i += 2
+        except:
+            HELP_COUNTER += 1
+            print_help()
+    if arg_d:
+        HELP_COUNTER += 1
+        print_help()
+    return var_d
+
+def print_help():
+    global HELP_COUNTER
+    if HELP_COUNTER == 1:
+        print "--name \tname of system"
+        print "--help \thelp\n"
+    sys.exit()
+
+def process_args(args):
+    global HELP_COUNTER
+    var_d = {}
+
+    arg_actions = {
+        'user-create' : user_create,
+        'user-delete' : user_delete,
+        'user-list' : user_list,
+        'user-get' : user_get,
+        'tenant-create' : tenant_create,
+        'tenant-delete' : tenant_delete,
+        'tenant-list' : tenant_list,
+        'tenant-get' : tenant_get,
+        'system-list' : system_list,
+        'system-get' : system_get,
+        'register-remote-system' : register_remote_system,
+        'deregister-remote-system' : deregister_remote_system,
+        'register-local-system' : register_local_system,
+        'deregister-local-system' : deregister_local_system
+    }
+
+    # get action
+    try:
+        action = args[0]
+        arg_actions[action](args[1:], var_d)
+    except:
+        HELP_COUNTER += 1
+        print_help()
+
+    return var_d
+
+def get_args():
+    args = []
+    for arg in sys.argv:
+        args.append(arg)
+    return args[1:]
+
+if __name__ == "__main__":
+    start_time = time.time()
+    args = get_args()
+    var_d = process_args(args)
+    print "Took",time.time() - start_time,"seconds to complete."
