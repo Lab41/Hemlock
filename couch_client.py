c70953ed2ee057eea804c0842d42737efa8be649
Charlie Lewis <clewis@iqt.org>
2013-05-15 21:11:52 +0000

    sample couch client

---
+++ couch_client.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import getpass, sys, time
+from couchbase.client import Couchbase
+
+def get_auth():
+    pw = getpass.getpass("Password:")
+    return pw
+
+def couch_server(server, bucket, pw):
+    # connect to the couch server
+    try:
+        c_server = Couchbase(server, bucket, pw)
+        c_bucket = c_server[bucket]
+    except:
+        print "Couch server failure"
+        sys.exit(0)
+    return c_server, c_bucket
+
+def print_help():
+    print "-b \t<bucket> (default is default)"
+    print "-s \t<couch server uri> (default is localhost:8091)"
+    print "-h \thelp\n"
+    sys.exit(0)
+
+def process_args(args):
+    # default initialization
+    server = "localhost:8091"
+    bucket = "default"
+
+    # process args
+    i = 0
+    while i < len(args):
+        if args[i] == "-s":
+            try:
+                server = args[i+1]
+                i += 1
+            except:
+                print_help()
+        elif args[i] == "-b":
+            try:
+                bucket = args[i+1]
+                i += 1
+            except:
+                print_help()
+        else:
+            print_help()
+        i += 1
+    return server, bucket
+
+def get_args():
+    args = []
+    for arg in sys.argv:
+        args.append(arg)
+    return args[1:]
+
+if __name__ == "__main__":
+    start_time = time.time()
+    args = get_args()
+    server, bucket = process_args(args)
+    pw = get_auth()
+    c_server, c_bucket = couch_server(server, bucket, pw)
+    #print c_bucket.get("357")[2]
+    print "Took",time.time() - start_time,"seconds to complete."
