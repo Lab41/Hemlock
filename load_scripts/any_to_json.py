64173eb15f61e54b275edd6937af08a17d4f2524
Charlie Lewis <clewis@iqt.org>
2013-05-31 11:26:58 -0700

    started script for loading generically any file in prep for couchbase

---
+++ load_scripts/any_to_json.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import fnmatch, os, sys, time, uuid
+
+def process_files(input):
+    matches = []
+    for root, dirnames, filenames in os.walk(input):
+        for filename in fnmatch.filter(filenames, '*.txt'):
+            matches.append(os.path.join(root, filename))
+    i = 0
+    for file in matches:
+        print file
+        i += 1
+    print i,"documents."
+
+def print_help():
+    print "-i \t<input path to files> (default is /mnt/)"
+    print "-h \thelp\n"
+    sys.exit(0)
+
+def process_args(args):
+    # default initialization
+    input = "/mnt/"
+
+    # process args
+    i = 0
+    while i < len(args):
+        if args[i] == "-i":
+            try:
+                input = args[i+1]
+                i += 1
+            except:
+                print_help()
+        else:
+            print_help()
+        i += 1
+    return input
+
+def get_args():
+    args = []
+    for arg in sys.argv:
+        args.append(arg)
+    return args[1:]
+
+if __name__ == "__main__":
+    start_time = time.time()
+    args = get_args()
+    input = process_args(args)
+    process_files(input)
+    print "Took",time.time() - start_time,"seconds to complete."
+
