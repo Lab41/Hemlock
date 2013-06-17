a23e25ebd0f7c3401eb2a60644a056bb3d6a66ff
Charlie Lewis <clewis@iqt.org>
2013-06-17 16:28:03 +0000

    test stubs

--- test_hemlock.py
+++ test_hemlock.py
@@ -1,6 +1,122 @@
 import hemlock
 
 class TestClass:
+    def process_deregister_local_system(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_deregister_remote_system(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_register_local_system(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_register_remote_system(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_role_create(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_role_delete(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_role_list(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_role_users_list(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_system_add_tenant(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_system_get(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_system_list(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_system_remove_tenant(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_system_tenants_list(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_tenant_create(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_tenant_delete(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_tenant_get(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_tenant_list(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_tenant_systems_list(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_tenant_users_list(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_user_add_role(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_user_add_tenant(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_user_create(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_user_delete(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_user_get(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_user_list(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_user_remove_role(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_user_remove_tenant(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_user_roles_list(self):
+        a = hemlock.Hemlock()
+        return
+
+    def process_user_tenants_list(self):
+        a = hemlock.Hemlock()
+        return
+
     def func(self, x):
         return x + 1
 
