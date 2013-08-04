d7b1c891d9f7c1d8217c3f9a168b8eedc99d88ae
clewis <clewis@iqt.org>
2013-08-04 11:30:15 -0700

    initial ODD stream client for Hemlock, not complete, server will take async requests from clients that connect

---
+++ clients/hodd.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import multiprocessing, sys
+from socket import *
+
+class HOdd:
+    def connect_client(self, client_dict):
+        c_server = ""
+        try:
+            sockobj = socket(AF_INET, SOCK_STREAM)
+            sockobj.bind(client_dict['HOST'], int(client_dict['PORT']))
+            sockobj.listen(2)
+            c_server = sockobj
+        except:
+            print "Failure connecting to the client server"
+            sys.exit(0)
+        return c_server
+
+    def worker(connection, address):
+        data_list = [[]]
+        desc_list = []
+        print 'Server connected by', address
+        while True:
+            data = connection.recv(1024)
+            if not data: break
+            print address, "got data:", data
+            #connection.send(data)
+        connection.close()
+
+        #data_list[0][i].append(str(record_dict[k]))
+        #desc_list[i].append([str(k)])
+
+        return data_list, desc_list
+
+    def get_data(self, client_dict, c_server, h_server, client_uuid):
+        jobs = []
+        while True:
+            connection, address = c_server.accept()
+            p = multiprocessing.Process(target=worker, args=(connection, address, ))
+            jobs.append(p)
+            p.start()
+
+            # !! TODO check if connection was closed, then return results
