4e2ff3d543f98b2461202c1f3a3ab79d4fa01288
clewis <clewis@iqt.org>
2013-08-12 14:52:31 -0700

    error fixed with h_server variable

--- clients/hemlock_base.py
+++ clients/hemlock_base.py
@@ -217,11 +217,12 @@ if __name__ == "__main__":
     c_server = c_inst.connect_client(client_dict)
     data_list = []
     desc_list = []
+    # !! TODO verify that connect_server with stream_worker doesn't break
+    h_server = hemlock.connect_server(server_dict)
     if not client.startswith("stream"):
         data_list, desc_list = c_inst.get_data(client_dict, c_server, h_server, client_uuid)
     else:
         hemlock.stream_workers()
-    h_server = hemlock.connect_server(server_dict)
     hemlock.verify_system(client_uuid)
     hemlock.send_data(data_list, desc_list, h_server, client_uuid)
     hemlock.update_hemlock(client_uuid, server_dict)
