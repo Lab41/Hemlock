bd71650ffc4f95fd221fceeb0f8c794b0d8a24e0
clewis <clewis@iqt.org>
2013-08-20 08:12:30 -0700

    added couchbase server install to build process cleaned up whitespace

--- hemlock/hemlock_options_parser.py
+++ hemlock/hemlock_options_parser.py
@@ -7,10 +7,10 @@ class PassThroughOptionParser(OptionParser):
     An unknown option pass-through implementation of OptionParser.
 
     When unknown arguments are encountered, bundle with largs and try again,
-    until rargs is depleted.  
+    until rargs is depleted.
 
     sys.exit(status) will still be called if a known argument is passed
-    incorrectly (e.g. missing arguments or bad argument types, etc.)        
+    incorrectly (e.g. missing arguments or bad argument types, etc.)
     """
     def _process_args(self, largs, rargs, values):
         while rargs:
--- scripts/ci/setup_env_hemlock.sh
+++ scripts/ci/setup_env_hemlock.sh
@@ -13,3 +13,12 @@ sudo pip install couchbase --upgrade
 
 # install mysql-python connector
 sudo apt-get install python-mysqldb
+
+# install Couchbase Server
+sudo wget http://packages.couchbase.com/releases/2.0.0/couchbase-server-enterprise_x86_64_2.0.0.deb
+sudo dpkg -i couchbase-server-enterprise_x86_64_2.0.0.deb
+sudo service couchbase-server start
+
+# setup bucket for test
+/opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-ramsize=256
+/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=hemlock --bucket-password=password --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=100 --bucket-replica=0 -u Administrator -p password
