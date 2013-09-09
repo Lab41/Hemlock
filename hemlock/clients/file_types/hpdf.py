3ca34293c27f899dd7535a5b3dc646eeff63390c
clewis <clewis@iqt.org>
2013-09-09 04:06:01 -0700

    moving out pdf from hfs

--- hemlock/clients/file_types/hpdf.py
+++ hemlock/clients/file_types/hpdf.py
@@ -14,6 +14,47 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+from hemlock.clients import hfs
+from pdfminer.pdfinterp import PDFResourceManager, process_pdf
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+
+import base64
+import json
+
 class Hpdf:
-    def __init__(self):
-        return
+    def process_files(self, debug, file, file_mime, h_server, client_uuid):
+        # !! TODO try/catch
+        f = open(file, 'rb')
+        h_inst = hfs.HFs()
+        # DEBUG
+        if file_mime:
+            if "pdf" in file_mime:
+                # DEBUG
+                try:
+                    text = self.convert_pdf(debug, file)
+                    j_str = json.dumps( { "payload" : text } )
+                except:
+                    b64_text = base64.b64encode(f.read())
+                    j_str = json.dumps( { "payload": b64_text } )
+                j_list.append(j_str)
+                h_inst.format_lists(debug, j_list, h_server, client_uuid)
+                j_list = []
+
+
+    def convert_pdf(self, debug, input):
+        # DEBUG
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+
+        fp = file(input, 'rb')
+        process_pdf(rsrcmgr, device, fp)
+        fp.close()
+        device.close()
+
+        str = retstr.getvalue()
+        retstr.close()
+        return str
