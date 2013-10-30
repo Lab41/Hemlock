74139bbc10980a1605a7e868f0d455b52f9689cc
clewis <clewis@iqt.org>
2013-10-29 18:03:35 -0700

    fixing python-magic

--- scripts/ci/setup_env_hemlock.sh
+++ scripts/ci/setup_env_hemlock.sh
@@ -28,8 +28,5 @@ sudo pip install couchbase --upgrade
 # install mysql-python connector
 sudo apt-get install -y python-mysqldb
 
-# install python-magic
-sudo apt-get install -y python-magic
-
 # install hemlock man page
 sudo cp docs/_build/man/hemlock.1 /usr/share/man/man1/hemlock.1.gz
