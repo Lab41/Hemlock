3034441460de1e222c0d797bc212ccbabd9dc00e
Charlie Lewis <clewis@iqt.org>
2013-11-06 03:22:54 +0000

    test hmysql

--- hemlock/test_hemlock.py
+++ hemlock/test_hemlock.py
@@ -1215,6 +1215,22 @@ class TestClass:
         a.start(0, "localhost", 50000, h_server, "asdf", 0, 0)
         return x, error
 
+    def process_hmysql_connect_client(self):
+        """
+        Tests hmysql connect_client.
+
+        :return: returns any data and a list of any errors
+        """
+        error = []
+        a = Hemlock_Base()
+        a.SERVER_CREDS_FILE = "hemlock/hemlock_creds_sample"
+        client_dict, server_dict = a.get_creds(0, "hemlock/clients/mysql_creds_sample")
+        h_server = a.connect_server(0, server_dict, 1)
+        a = HMysql()
+        c_server = a.connect_client(0, client_dict)
+        x = a.get_data(0, client_dict, c_server, h_server, "asdf", 0)
+        return x, error
+
     def process_hemlock_runner_mysql_server(self):
         """
         Tests hemlock_runner mysql_server.
@@ -1849,6 +1865,13 @@ class TestClass:
         with pytest.raises(SystemExit):
             x, error = self.process_hemlock_runner_mysql_server2()
             for err in error: assert err == 0
+
+    def test_process_hmysql_connect_client(self):
+        """
+        Calls the test function for the hmysql connect_client.
+        """
+        x, error = self.process_hmysql_connect_client()
+        for err in error: assert err == 0
 
     def test_process_hemlock_get_auth(self, server_mysql, database, mysql_username, mysql_password, couchbase_server, couchbase_bucket, couchbase_username, couchbase_password, elasticsearch_endpoint, hemlock_debug, no_couchbase, hemlock_version):
         """
