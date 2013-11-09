b141e8ba8dedce821dc47655aa22ec31be1ef2dc
clewis <clewis@iqt.org>
2013-11-09 08:56:05 -0800

    typo

--- hemlock/test_hemlock.py
+++ hemlock/test_hemlock.py
@@ -1436,7 +1436,7 @@ class TestClass:
         """
         Ensures that mongo is running on the server running the tests.
         """
-        connection = pymongo.Connection(localhost, 27017)
+        connection = pymongo.Connection("localhost", 27017)
         db = connection['test-database']
         collection = db['test-collection']
         assert 1
