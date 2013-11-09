53a573a46a30be2e3e2483faff3b429c35e2cbdf
clewis <clewis@iqt.org>
2013-11-09 09:21:17 -0800

    putting port back

--- hemlock/test_hemlock.py
+++ hemlock/test_hemlock.py
@@ -1436,7 +1436,7 @@ class TestClass:
         """
         Ensures that mongo is running on the server running the tests.
         """
-        connection = pymongo.MongoClient("127.0.0.1", 27018)
+        connection = pymongo.MongoClient("127.0.0.1", 27017)
         db = connection['test-database']
         collection = db['test-collection']
         assert 1
