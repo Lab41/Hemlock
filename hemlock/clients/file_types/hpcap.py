24f85ae65471f3d1cf56bb35aa3f0a492a4968c1
Charlie Lewis <charliel@lab41.org>
2014-03-20 11:02:55 -0700

    initialize j_list before assignment

--- hemlock/clients/file_types/hpcap.py
+++ hemlock/clients/file_types/hpcap.py
@@ -21,6 +21,7 @@ import json
 
 class Hpcap:
     def process_files(self, debug, file, file_mime, h_server, client_uuid):
+        j_list = []
         # !! TODO try/catch
         f = open(file, 'rb')
         h_inst = hfs.HFs()
